1d7866b7b59bd8ae88d89e06ff208a1bb2326efe
Boris Grunwald <borisgrunwald@gmail.com>
2021-12-31 13:58:55 +0100

    added global styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,21 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`  
 
+body, html { 
+  font-family: 'Roboto', sans-serif;
+}
+
+ * {
+    margin: 0;
+    padding: 0;
+    box-sizing:border-box;
+ }
+
+  button {
+      border: none;
+      font-family:inherit
+      cursor: pointer;
+  }
 `
 
 function App() {
