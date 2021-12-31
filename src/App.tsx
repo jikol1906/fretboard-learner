3c2ba0b05ada87127c3bf5becba1f7a8f5b9eead
Boris Grunwald <borisgrunwald@gmail.com>
2021-12-31 13:59:35 +0100

    App.tsx: Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
 import StartButton from './features/StartButton/StartButton';
 import { createGlobalStyle } from 'styled-components';
 
