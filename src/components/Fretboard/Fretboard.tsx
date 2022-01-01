4960f1d12f698ba130f35b0686f0d820fdf06e87
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-01 18:44:23 +0100

    Fretboard.tsx: used css property for rotation

--- src/components/Fretboard/Fretboard.tsx
+++ src/components/Fretboard/Fretboard.tsx
@@ -15,8 +15,10 @@ const Fretboard: React.FunctionComponent<IFretboardProps> = (props) => {
         return <Pointer style={styles} key={`${x}${y}`}/>
     })
 
+    const styles = {'--rotation':`${rotation}deg`} as React.CSSProperties
+
     return (
-        <FretboardContainer rotation={rotation}>
+        <FretboardContainer style={styles}>
             <svg style={{width:'100%',height:'auto'}} id="fretboard" viewBox="0 0 1020 256" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="4" width="1012" height="256" rx="17" fill="url(#paint0_linear_2025_1202)" />
                 <rect x="87" width="2" height="256" fill="#A8A8A8" />
--- src/components/Fretboard/FretboardStyles.ts
+++ src/components/Fretboard/FretboardStyles.ts
@@ -1,16 +1,14 @@
 import styled, { keyframes } from "styled-components";
 
-interface FretboardContainerProps {
-    readonly rotation: number;
-}
 
-export const FretboardContainer = styled.div<FretboardContainerProps>`
+
+export const FretboardContainer = styled.div`
     margin: 0 auto;
     margin-bottom: 4em;
     width: 63.25em;
     position: relative;
     font-size: min(1.3vw,1rem);
-    transform: perspective(550px) rotateX(${props => props.rotation}deg);
+    transform: perspective(550px) rotateX(var(--rotation));
 `
 
 const pulse = keyframes`
