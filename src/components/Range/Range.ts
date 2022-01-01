507cc8ced3828953486b28e0674278785af15f1c
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-01 12:21:08 +0100

    Added range component

---
+++ src/components/Range/Range.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Range = styled.input`
+
+    -webkit-appearance: none;
+    border-radius: 50px;
+    width: 100%;
+    height: 1.56em;
+    background: #d3d3d3;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    -webkit-transition: opacity .2s;
+    -o-transition: opacity .2s;
+    transition: opacity .2s;
+
+
+    &:hover {
+        opacity: 1;
+    }
+
+    &::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        appearance: none;
+        width: 2.5em;
+        height: 2.5em;
+        background: #52a5d6;
+        cursor: pointer;
+        border-radius: 50px;
+    }
+
+    &::-moz-range-thumb {
+        border-radius: 50px;
+        width: 2.5em;
+        height: 2.5em;
+        background: #52a5d6;
+        cursor: pointer;
+    }
+`
