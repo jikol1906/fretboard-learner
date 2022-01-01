5fc7ac3a6cf0cc7f567a72519c3a94ac66bd40f6
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-01 14:55:25 +0100

    start game on click startbutton

--- src/containers/Menu/Menu.tsx
+++ src/containers/Menu/Menu.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
-import StartButton from '../../components/StartButton/StartButton';
+import { StartButton } from '../../components/StartButton/StartButton';
+import { selectFretboardRotation, selectTimeBetween, setFretboardRotation, setGameStarted, setTimeBetween } from '../../redux/appSlice';
 import { StyledMenu } from './MenuStyles';
 
 interface IMenuProps {
 }
 
 const Menu: React.FunctionComponent<IMenuProps> = (props) => {
+  const dispatch = useAppDispatch();
   return (
       <StyledMenu>
-          <StartButton>START</StartButton>
+          <StartButton onClick={e => dispatch(setGameStarted(true))}>START</StartButton>
       </StyledMenu>
   );
 };
