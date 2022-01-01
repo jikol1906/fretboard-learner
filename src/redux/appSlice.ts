e9366ecab73835bebf97158677842634261857ae
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-01 13:10:43 +0100

    apiSlice.ts: added fretboard rotation

--- src/redux/appSlice.ts
+++ src/redux/appSlice.ts
@@ -6,12 +6,12 @@ import { RootState } from "../app/store";
 
 export interface AppState {
   timeBetween: number;
-  
+  fretboardRotation:number;
 }
 
 const initialState: AppState = {
   timeBetween: 0,
-  
+  fretboardRotation:0
 };
 
 
@@ -23,16 +23,20 @@ export const appSlice = createSlice({
   reducers: {
     setTimeBetween(state,action: PayloadAction<number>) {
       state.timeBetween = action.payload
-    }
+    },
+    setFretboardRotation(state,action: PayloadAction<number>) {
+      state.fretboardRotation = action.payload
+    },
   },
 });
 
-export const { setTimeBetween } = appSlice.actions;
+export const { setTimeBetween,setFretboardRotation } = appSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectTimeBetween = (state: RootState) => state.app.timeBetween;
+export const selectFretboardRotation = (state: RootState) => state.app.fretboardRotation;
 
 
 
