a7b08d1d68ade129da888158718bfea3dfaf98d6
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-03 09:41:42 +0100

    set buttons text to redux state and added clickhandler

--- src/components/NoteButtons/NotesButtons.tsx
+++ src/components/NoteButtons/NotesButtons.tsx
@@ -1,16 +1,29 @@
 import * as React from 'react';
+import { useAppSelector } from '../../app/hooks';
+import { selectNoteButtons } from '../../redux/appSlice';
 import { NoteButton, NoteButtonsContainer } from './NoteButtonsStyles';
 
 interface INoteButtonsProps {
 }
 
 const NoteButtons: React.FunctionComponent<INoteButtonsProps> = (props) => {
+
+  const [b1,b2,b3,b4] = useAppSelector(selectNoteButtons);
+
+  const btnClicked = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      const b = e.target as HTMLButtonElement
+
+      
+      
+      
+  }
+
   return (
       <NoteButtonsContainer style={{gridArea:'a2'}}>
-          <NoteButton style={{gridArea:'b1'}}>G#/Ab</NoteButton>
-          <NoteButton style={{gridArea:'b2'}}>B</NoteButton>
-          <NoteButton style={{gridArea:'b3'}}>C</NoteButton>
-          <NoteButton style={{gridArea:'b4'}}>D</NoteButton>
+          <NoteButton onClick={btnClicked} style={{gridArea:'b1'}}>{b1}</NoteButton>
+          <NoteButton onClick={btnClicked} style={{gridArea:'b2'}}>{b2}</NoteButton>
+          <NoteButton onClick={btnClicked} style={{gridArea:'b3'}}>{b3}</NoteButton>
+          <NoteButton onClick={btnClicked} style={{gridArea:'b4'}}>{b4}</NoteButton>
       </NoteButtonsContainer>
   );
 };
