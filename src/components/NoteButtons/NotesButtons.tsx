2f7c57e93fbac997b43d42778022f55ffe11c1ed
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-03 14:29:23 +0100

    use buttons 1,2,3,4 to click on notebuttons

--- src/components/NoteButtons/NotesButtons.tsx
+++ src/components/NoteButtons/NotesButtons.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { incrementCorrectAnswered, incrementTotalAnswered, selectCorrectAnswer, selectNoteButtons, selectWrongAnswerClicked, setCorrectAnswer, setWrongAnswerClicked } from '../../redux/appSlice';
 import { NoteButton, NoteButtonsContainer } from './NoteButtonsStyles';
@@ -13,12 +14,9 @@ const NoteButtons: React.FunctionComponent<INoteButtonsProps> = (props) => {
   const correctAnswer = useAppSelector(selectCorrectAnswer);
   const wrongAnswerClicked = useAppSelector(selectWrongAnswerClicked);
 
-  const btnClicked = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-      const b = e.target as HTMLButtonElement
-      const bValue = b.innerText;
-      console.log('here',correctAnswer,bValue);
-      
-      if(bValue === correctAnswer) {
+  const btnClicked = (val:string) => {
+    
+      if(val === correctAnswer) {
         dispatch(setWrongAnswerClicked(false))
         dispatch(incrementCorrectAnswered())
         dispatch(incrementTotalAnswered())
@@ -29,6 +27,29 @@ const NoteButtons: React.FunctionComponent<INoteButtonsProps> = (props) => {
       
   }
 
+  useEffect(() => {
+    const handleKeydown = (ev:KeyboardEvent) => {
+      switch(ev.key) {
+        case '1':         
+          btnClicked(b1)
+          break
+        case '2': 
+          btnClicked(b2)
+          break
+        case '3': 
+          btnClicked(b3)
+          break
+        case '4': 
+          btnClicked(b4)
+      }
+    }
+    document.addEventListener('keydown',handleKeydown);
+
+    return () => {
+      document.removeEventListener('keydown',handleKeydown)
+    }
+  },[b1,b2,b3,b4])
+  
   return (
       <NoteButtonsContainer style={{gridArea:'a2'}}>
           <NoteButton onClick={btnClicked} style={{gridArea:'b1'}}>{b1}</NoteButton>
