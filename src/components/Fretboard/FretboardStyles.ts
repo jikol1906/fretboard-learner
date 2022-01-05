e464b126764d57ba133c138fe364d714c74779f7
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-05 20:38:24 +0100

    increased fretboard bottom margin

--- src/components/Fretboard/FretboardStyles.ts
+++ src/components/Fretboard/FretboardStyles.ts
@@ -4,7 +4,7 @@ import styled, { keyframes } from "styled-components";
 
 export const FretboardContainer = styled.div`
     margin: 0 auto;
-    margin-bottom: 3em;
+    margin-bottom: 4em;
     width: 63.25em;
     position: relative;
     font-size: min(1.3vw,1rem);
