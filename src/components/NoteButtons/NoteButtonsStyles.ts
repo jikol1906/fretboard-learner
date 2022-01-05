b34d6430b34874e06e1c79e302111020da041523
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-05 21:13:57 +0100

    added numbers under notebuttons

--- src/components/NoteButtons/NoteButtonsStyles.ts
+++ src/components/NoteButtons/NoteButtonsStyles.ts
@@ -15,6 +15,7 @@ interface NoteButtonProps {
 }
 
 export const NoteButton = styled.button<NoteButtonProps>`
+    position: relative;
     font-size:2.5em;
     font-weight: 400;
     box-sizing: content-box;
@@ -27,5 +28,20 @@ export const NoteButton = styled.button<NoteButtonProps>`
     &:not(:last-child) {
         border-right: 0.083em solid rgba(255,255,255,.4);
     }
+
+    &::after {
+        position: absolute;
+        box-sizing: border-box;
+        color:var(--color-white);
+        border-radius: 100px;
+        left:50%;
+        top:120%;
+        transform: translateX(-50%);
+        font-size: .5em;
+    }
+    &:nth-child(1)::after {content: "1";}
+    &:nth-child(2)::after {content: "2";}
+    &:nth-child(3)::after {content: "3";}
+    &:nth-child(4)::after {content: "4";}
 `
 
