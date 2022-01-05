9eccc510c4f6e8fbd53803f2a63c233ea32ae2a4
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-05 20:50:12 +0100

    NoteButtons.tsx use buttonrefs to click buttons

--- src/components/NoteButtons/NotesButtons.tsx
+++ src/components/NoteButtons/NotesButtons.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { incrementCorrectAnswered, incrementTotalAnswered, selectCorrectAnswer, selectNoteButtons, selectWrongAnswerClicked, setCorrectAnswer, setWrongAnswerClicked } from '../../redux/appSlice';
 import { NoteButton, NoteButtonsContainer } from './NoteButtonsStyles';
@@ -13,6 +13,12 @@ const NoteButtons: React.FunctionComponent<INoteButtonsProps> = (props) => {
   const [b1,b2,b3,b4] = useAppSelector(selectNoteButtons);
   const correctAnswer = useAppSelector(selectCorrectAnswer);
   const wrongAnswerClicked = useAppSelector(selectWrongAnswerClicked);
+  
+  const btn1ref = useRef<HTMLButtonElement>(null)
+  const btn2ref = useRef<HTMLButtonElement>(null)
+  const btn3ref = useRef<HTMLButtonElement>(null)
+  const btn4ref = useRef<HTMLButtonElement>(null)
+  
 
   const btnClicked = (val:string) => {
     
@@ -35,16 +41,16 @@ const NoteButtons: React.FunctionComponent<INoteButtonsProps> = (props) => {
     const handleKeydown = (ev:KeyboardEvent) => {
       switch(ev.key) {
         case '1':         
-          btnClicked(b1)
+          btn1ref.current?.click()
           break
         case '2': 
-          btnClicked(b2)
+          btn2ref.current?.click()
           break
         case '3': 
-          btnClicked(b3)
+          btn3ref.current?.click()
           break
         case '4': 
-          btnClicked(b4)
+          btn4ref.current?.click()
       }
     }
     document.addEventListener('keydown',handleKeydown);
@@ -56,10 +62,10 @@ const NoteButtons: React.FunctionComponent<INoteButtonsProps> = (props) => {
   
   return (
       <NoteButtonsContainer style={{gridArea:'a2'}}>
-          <NoteButton wrong={wrongAnswerClicked} onClick={() => btnClicked(b1)} style={{gridArea:'b1'}}>{b1}</NoteButton>
-          <NoteButton wrong={wrongAnswerClicked} onClick={() => btnClicked(b2)} style={{gridArea:'b2'}}>{b2}</NoteButton>
-          <NoteButton wrong={wrongAnswerClicked} onClick={() => btnClicked(b3)} style={{gridArea:'b3'}}>{b3}</NoteButton>
-          <NoteButton wrong={wrongAnswerClicked} onClick={() => btnClicked(b4)} style={{gridArea:'b4'}}>{b4}</NoteButton>
+          <NoteButton ref={btn1ref} wrong={wrongAnswerClicked} onClick={() => btnClicked(b1)} style={{gridArea:'b1'}}>{b1}</NoteButton>
+          <NoteButton ref={btn2ref} wrong={wrongAnswerClicked} onClick={() => btnClicked(b2)} style={{gridArea:'b2'}}>{b2}</NoteButton>
+          <NoteButton ref={btn3ref} wrong={wrongAnswerClicked} onClick={() => btnClicked(b3)} style={{gridArea:'b3'}}>{b3}</NoteButton>
+          <NoteButton ref={btn4ref} wrong={wrongAnswerClicked} onClick={() => btnClicked(b4)} style={{gridArea:'b4'}}>{b4}</NoteButton>
       </NoteButtonsContainer>
   );
 };
