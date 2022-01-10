27c7affdba2a07301fbb1629a57d47724ebbe2e6
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-10 20:49:58 +0100

    added checkbox in meny

--- src/containers/Menu/Menu.tsx
+++ src/containers/Menu/Menu.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import Checkbox from '../../components/Checkbox/Checkbox';
 import { Label } from '../../components/Label/Label';
 import { Range } from '../../components/Range/Range';
 import { StartButton } from '../../components/StartButton/StartButton';
-import { selectFretboardRotation, selectTimeBetween, setFretboardRotation, setGameStarted, setTimeBetween } from '../../redux/appSlice';
+import { selectFretboardRotation, selectTimeBetween, setFretboardRotation, setGameStarted, setPracticemode, setTimeBetween } from '../../redux/appSlice';
 import { StyledMenu } from './MenuStyles';
 
 interface IMenuProps {
@@ -23,7 +24,11 @@ const Menu: React.FunctionComponent<IMenuProps> = (props) => {
             <Label htmlFor="rotation">Fretboard rotation</Label>
             <Range  onChange={e => dispatch(setFretboardRotation(+e.target.value))} type="range" min="0" max="70" value={rotation} step="1" id="rotation"/>
           </div>
-          <div style={{gridArea:'a3'}}>
+          <div style={{gridArea:'a3', textAlign:'center'}}>
+            <Label htmlFor="checkbox">Practice mode</Label>
+            <Checkbox id="checkbox" onChange={(e) => dispatch(setPracticemode(e.target.checked))}/>
+          </div>
+          <div style={{gridArea:'a4'}}>
             <Label htmlFor="timebetween">Time between notes: {timebetween / 10}</Label>  
             <Range  onChange={e => dispatch(setTimeBetween(+e.target.value))} type="range" min="10" max="100" value={timebetween} step="5" id="timebetween"/>
           </div>
--- src/containers/Menu/MenuStyles.ts
+++ src/containers/Menu/MenuStyles.ts
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 export const StyledMenu = styled.div`
     display: grid;
+    padding: 0 2em;
     grid-template-areas: 
-        "a1 a1"
-        "a2 a3";
+        "a1 a1 a1"
+        "a2 a3 a4";
+    grid-template-columns: repeat(3,1fr);
     justify-items: center;
     justify-content: center;
     font-size: min(1.7vw,1rem);
     grid-row-gap: 3em;
-    grid-column-gap: 9em;
+    grid-column-gap: 3em;
 `
