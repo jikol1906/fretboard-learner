42b3a014f9d357f537832a977e85a25b7740de54
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-11 19:23:50 +0100

    Added button margin

--- src/containers/Menu/Menu.tsx
+++ src/containers/Menu/Menu.tsx
@@ -21,7 +21,7 @@ const Menu: React.FunctionComponent<IMenuProps> = (props) => {
       <StyledMenu>
           <Label style={{gridArea:'l1'}} htmlFor="rotation">Fretboard rotation</Label>
           <Range  style={{gridArea:'i1'}} onChange={e => dispatch(setFretboardRotation(+e.target.value))} type="range" min="0" max="70" value={rotation} step="1" id="rotation"/>
-          <Button style={{gridArea:'a2', marginLeft:'1.5em'}} fontSize='1.5em' onClick={e => dispatch(setGameStarted(true))}>START</Button>
+          <Button style={{gridArea:'a2'}} ml={[null,'2em']} fontSize='1.5em' onClick={e => dispatch(setGameStarted(true))}>START</Button>
           <Label style={{gridArea:'l2'}} htmlFor="checkbox">Practice mode</Label>
           <Checkbox style={{gridArea:'i2'}} id="checkbox" onChange={(e) => dispatch(setPracticemode(e.target.checked))}/>
       </StyledMenu>
