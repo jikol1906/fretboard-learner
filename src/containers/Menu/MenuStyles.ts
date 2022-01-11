5ae87ad9bb00732325be025c9adad74011b90a1f
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-11 19:14:19 +0100

    added breakpoint to menustyles

--- src/containers/Menu/MenuStyles.ts
+++ src/containers/Menu/MenuStyles.ts
@@ -4,14 +4,23 @@ export const StyledMenu = styled.div`
     display: grid;
     padding: 0 2em;
     grid-template-areas: 
-        "l1 a2"
-        "i1 a2"
-        "l2 a2"
-        "i2 a2";
+    "l1 a2"
+    "i1 a2"
+    "l2 a2"
+    "i2 a2";
     justify-items: center;
     align-items: center;
     justify-content: center;
-    font-size: min(1.7vw,1rem);
+    font-size: clamp(.6rem,1.7vw,1rem);
     grid-row-gap: 2em;
     grid-column-gap: 4em;
+    
+    @media only screen and (max-width:600px) {
+        grid-template-areas: 
+                "a2"
+                "l1"
+                "i1"
+                "l2"
+                "i2";
+    }
 `
