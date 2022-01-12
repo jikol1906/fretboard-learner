b57040b51f0f1e8e0da7dfcaad54f663bc16af41
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-12 15:14:17 +0100

    added dimWhite style

--- src/Styles/Theme.ts
+++ src/Styles/Theme.ts
@@ -1,6 +1,7 @@
 const breakpoints = ['40em', '52em', '64em', '80em']
 const colors = {
     white:"#fff",
+    dimWhite:"rgba(255,255,255,.4)",
     grey:"#d3d3d3",
     greySecondary:"#696969",
     primary:"#02AAB0",
