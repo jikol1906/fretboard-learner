bb12824b7e879a87abf37ccac58b14c227a44fbf
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-12 20:37:16 +0100

    removed unecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useAppDispatch, useAppSelector } from './app/hooks';
+import { useAppSelector } from './app/hooks';
 import Fretboard from './components/Fretboard/Fretboard';
-import { StopButton } from './components/StopButton/StopButtonStyles';
 import Game from './containers/Game/Game';
 import Menu from './containers/Menu/Menu';
 import { GlobalStyle } from './Styles/GlobalStyle';
-import { selectGameStarted, setGameStarted } from './redux/appSlice';
+import { selectGameStarted } from './redux/appSlice';
 
 function App() {
   const gameStarted = useAppSelector(selectGameStarted);
