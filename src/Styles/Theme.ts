08ff52da51c2eac31e86b3d70081dbb4c663a7bc
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-12 11:56:30 +0100

    added colors to Theme

--- src/Styles/Theme.ts
+++ src/Styles/Theme.ts
@@ -1,6 +1,15 @@
 const breakpoints = ['40em', '52em', '64em', '80em']
+const colors = {
+    white:"#fff",
+    grey:"#d3d3d3",
+    greySecondary:"#696969",
+    primary:"#02AAB0",
+    secondary:"#00CDAC",
+    tertiary:"#00a0ff",
+}
 
 
 export default {
-    breakpoints
+    breakpoints,
+    colors
 }
