49adac0b3d1f00b3ab0524a4c412223712290f28
Boris Grunwald <borisgrunwald@gmail.com>
2022-01-13 12:45:48 +0100

    added tabindex to button

--- src/containers/Menu/Menu.tsx
+++ src/containers/Menu/Menu.tsx
@@ -45,7 +45,7 @@ const Menu: React.FunctionComponent<IMenuProps> = (props) => {
       > 
           <Label gridArea="l1" htmlFor="rotation">Fretboard rotation</Label>
           <Range  gridArea="i1" onChange={e => dispatch(setFretboardRotation(+e.target.value))} type="range" min="0" max="70" value={rotation} step="1" id="rotation"/>
-          <Button fontSize="2em" style={{gridArea:'a2', letterSpacing:"4px"}} onClick={e => dispatch(setGameStarted(true))}>START</Button>
+          <Button tabIndex={1} fontSize="2em" style={{gridArea:'a2', letterSpacing:"4px"}} onClick={e => dispatch(setGameStarted(true))}>START</Button>
           <Label gridArea="l2" htmlFor="checkbox">Practice mode</Label>
           <Checkbox style={{gridArea:'i2'}} checked={isPracticeMode} id="checkbox" onChange={(e) => dispatch(setPracticemode(e.target.checked))}/>
       </Grid>
